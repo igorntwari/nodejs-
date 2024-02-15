57ae63846cbf2cf10e4f03dbf354c47355bfd47f
igorntwari <igorntwari28@gmail.com>
2024-02-15 11:12:23 +0200

    added pure node js

---
+++ servers/server.js
@@ -0,0 +1,41 @@
+const http = require("http");
+const fs = require("fs");
+const { error } = require("console");
+const server = http.createServer((req, res) => {
+  res.setHeader("Content-Type", "text/html");
+  let path = "./views/";
+
+  switch (req.url) {
+    case "/":
+      path += "/index.html";
+      res.statusCode = 200;
+      break;
+
+    case "/about":
+      path += "/about.html";
+      res.statusCode = 200;
+      break;
+    case "/about-me":
+      res.setHeader("Location", "/about");
+      res.statusCode = 301;
+      res.end();
+      break;
+    default:
+      path += "./404.html";
+      res.statusCode = 404;
+      break;
+  }
+  fs.readFile(path, (error, data) => {
+    if (error) {
+      console.log(error);
+      res.end();
+    } else {
+      //   res.write(data);
+      res.end(data);
+    }
+  });
+});
+
+server.listen(3000, "localhost", () => {
+  console.log("server started to listen to port 3000");
+});
