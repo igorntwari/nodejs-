ff0717a682d5a561a87acce52e92b54847ee1838
igorntwari <igorntwari28@gmail.com>
2024-03-26 10:51:21 +0200

    feat:adding authantication

--- E-green/Backend/Auth/AuthRoutes.js
+++ E-green/Backend/Auth/AuthRoutes.js
@@ -1,12 +1,48 @@
 const express = require("express");
-const User = require("../Models/user");
-
 const router = express.Router();
+const bcrypt = require("bcrypt");
+const User = require("../Models/user");
 
 router.post("/signup", async (req, res) => {
   const { username, password, email } = req.body;
-  const LoggedInUser = await User.create({ username, password, email });
-  return res.status(201).json(LoggedInUser);
+
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const LoggedInUser = await User.create({
+      username,
+      password: hashedPassword,
+      email,
+    });
+    console.log("new user signed up");
+    return res.status(201).json(LoggedInUser);
+  } catch (error) {
+    console.error(error);
+    return res
+      .status(500)
+      .json({ message: "An error occurred while signing up" });
+  }
 });
 
-module.exports = router
+router.post("/login", async (req, res) => {
+ const { username, password } = req.body;
+
+ try {
+    const LoggedInUser = await User.findOne({ username });
+    if (!LoggedInUser) {
+      return res.status(404).json({ error: "User not found" });
+    }
+    const isMatch = await bcrypt.compare(password, LoggedInUser.password);
+    if (isMatch) {
+      return res.status(200).json(LoggedInUser);
+    } else {
+      return res.status(401).json({ error: "Invalid credentials" });
+    }
+ } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "An error occurred while logging in" });
+ }
+});
+
+
+module.exports = router;
